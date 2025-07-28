c91b01ce29f492828b2c708787a2044272462db4
aali583 <aali583@wgu.edu>
2025-07-28 17:27:37 -0400

    Add interactive SVG map logic

--- world-map/src/app/svg-map/svg-map.ts
+++ world-map/src/app/svg-map/svg-map.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-svg-map',
-  imports: [],
-  templateUrl: './svg-map.html',
-  styleUrl: './svg-map.css'
+  templateUrl: './svg-map.component.html',
+  styleUrls: ['./svg-map.component.css']
 })
-export class SvgMap {
+export class SvgMapComponent {
+  @Output() countrySelected = new EventEmitter<string>();
 
+  highlight(event: any) {
+    event.target.style.fill = '#5EDFFF';
+  }
+  unhighlight(event: any) {
+    event.target.style.fill = '';
+  }
+  selectCountry(code: string) {
+    this.countrySelected.emit(code);
+  }
 }
