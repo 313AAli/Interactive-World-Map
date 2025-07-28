3523570c1da1a85d6011c485f0b2fd900b1b2e24
aali583 <aali583@wgu.edu>
2025-07-28 17:01:25 -0400

    Add map-view and svg-map components

---
+++ world-map/src/app/svg-map/svg-map.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-svg-map',
+  imports: [],
+  templateUrl: './svg-map.html',
+  styleUrl: './svg-map.css'
+})
+export class SvgMap {
+
+}
